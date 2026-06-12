55e339bf42e1862710e9b5209ddd2d2ec81df4e7
hoony6555 <misohoon6555@gmail.com>
2026-06-12 12:47:15 +0900

    의존성 3개 삭제

--- spring-server/fx_bank/src/main/java/com/example/bank/fx/controller/FxMainController.java
+++ spring-server/fx_bank/src/main/java/com/example/bank/fx/controller/FxMainController.java
@@ -2,6 +2,9 @@ package com.example.bank.fx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class FxMainController {
@@ -9,7 +12,7 @@ public class FxMainController {
 	@GetMapping("/")
 	public String fxHome() {
 		
-		return "fxHome";
+		return "fx-home";
 	}
 	
 }
---
+++ spring-server/fx_bank/src/main/java/com/example/bank/fx/service/FxService.java
@@ -0,0 +1,5 @@
+package com.example.bank.fx.service;
+
+public class FxService {
+
+}
