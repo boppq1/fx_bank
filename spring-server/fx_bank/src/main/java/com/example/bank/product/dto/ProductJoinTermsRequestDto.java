97a6551d8159a72e3908639c4f868dc5a6553238
이민주 <minju200308@gmail.com>
2026-06-17 15:48:04 +0900

    악

--- spring-server/fx_bank/src/main/java/com/example/bank/product/dto/ProductJoinTermsRequestDto.java
+++ spring-server/fx_bank/src/main/java/com/example/bank/product/dto/ProductJoinTermsRequestDto.java
@@ -1,5 +1,13 @@
 package com.example.bank.product.dto;
+// 약관 공의 화면에서 체크한 값 받을 때 사용
 
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class ProductJoinTermsRequestDto {
-
+	private Long productNo;
+	private List<String> requiredTermsCodes; // 사용자가 동의한 필수약관 코드 목록
+	private List<String> optionalTermsCodes; // 사용자가 동의한 선택약관 코드 목록
 }
